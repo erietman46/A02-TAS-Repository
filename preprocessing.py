295859503acb0e55706898a5675ff5aae83a07c8
yoskaem <kaempfyoshua@gmail.com>
2026-03-05 12:12:07 +0100

    Added preprocessing.py for preprocessing the data with a short outline

---
+++ preprocessing.py
@@ -0,0 +1,7 @@
+'''
+This file will preprocess the data to be used in the time-domain analysis.
+Primarily, it will:
+1. Extract the needed arrays from the data
+2. Calculate the mean over the various repetitions
+3. Present the various pilots and conditions in a clear format for further analysis
+'''
