dc4c6e5610db90a4cee6e05e8afb6698c94e5b87
Javier Torroja <torroja.javier@gmail.com>
2026-03-12 11:29:22 +0100

    put data into array

--- data_preprocess.py
+++ data_preprocess.py
@@ -9,3 +9,9 @@ data = np.array(data)
 for subject in range(1, 7):
     for condition in range(1, 7):
         data[subject-1][condition-1] = np.load(path / f"ae2224I_measurement_data_subj{subject}_C{condition}.npz")
+
+for subject in range(1, 7):
+    for condition in range(1, 7):
+        npz_file = data[subject-1][condition-1]
+        data[subject-1][condition-1] = {key: npz_file[key] for key in npz_file.files}
+
