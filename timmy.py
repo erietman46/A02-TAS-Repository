9f05dc351a96c7d5986ed31fd898cc0857cc9d18
Can <canfaks@gmail.com>
2026-03-12 08:52:38 +0100

    graphs

--- timmy.py
+++ timmy.py
@@ -4,11 +4,11 @@
 #import matplotlib.pyplot as plt 
 
 
-#data = sc.loadmat("ae2224I_measurement_data_subj1_C1.mat")
-#t    = data["t"][0]
-#e    = data["e"]
-#e1   = e[:,0]
+data = sc.loadmat("ae2224I_measurement_data_subj1_C1.mat")
+t    = data["t"][0]
+e    = data["e"]
+e1   = e[:,0]
 
 
-#plt.plot(t,e1)
-#plt.show()
+plt.plot(t,e1)
+plt.show()
