b870df5c1c82623668c58d8e74d270b7896e163d
yoskaem <kaempfyoshua@gmail.com>
2026-03-12 11:34:30 +0100

    finished getting all crossings

--- zero_crossings.py
+++ zero_crossings.py
@@ -1,19 +1,19 @@
 from preprocessing import pilots
 import numpy as np
 
-pilot1 = pilots[0]
-C1 = pilot1["C1"]
-e = C1["e"]
-e = np.array(e)
-e_transposed = e.T
-
-crossings = 0
-for index, i in enumerate(e_transposed):
-    for j in range(len(i) - 1):
-        current = i[j]
-        next = i[j + 1]
-        if current * next < 0:
-            crossings += 1
-crossings = crossings/5
+crossings = [[0]*6 for _ in range(6)]
+for index, pilot in enumerate(pilots):
+    for index2, condition in enumerate(pilot.values()):
+        e = condition["e"]
+        e = np.array(e)
+        e_transposed = e.T
+        cross = 0
+        for i in e_transposed:
+            for j in range(len(i) - 1):
+                current = i[j]
+                next = i[j + 1]
+                if current * next < 0:
+                    cross += 1
+        crossings[index][index2] = cross/5
 
 print(crossings)
