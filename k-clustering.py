0541e43820d927c1ebb5ea048c43f8a1c268e782
Javier Torroja <torroja.javier@gmail.com>
2026-03-17 16:56:40 +0100

    k clustering

--- k-clustering.py
+++ k-clustering.py
@@ -6,4 +6,4 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, linkage
 
-single_linkage_model = AgglomerativeClustering(linkage='single', distance_threshold =0, n_clusters=None)
+combinations = (("e", "u"), ("e", "u", "de"), ("e", "u", "du"), ("e", "u", "de", "du"))
