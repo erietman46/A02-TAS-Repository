19a41c710a4f512b7b5152b798e5baf7978db176
Javier Torroja <torroja.javier@gmail.com>
2026-03-17 17:24:22 +0100

    k means

--- k-clustering.py
+++ k-clustering.py
@@ -6,6 +6,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, linkage
 
+for subject in range(1, 7)
+    for condition in range(1, 4)
+        data = np.load(f"ae2224I_measurement_data_subj{subject}_C{condition}.npz")
+        data_e = data["e"]
+        data_u = data["u"]
+        data_t = data["t"]
+        f"X{subject}_C{condition}" = data['Hpe_FC']
+
 combinations = (("e", "u"), ("e", "u", "de"), ("e", "u", "du"), ("e", "u", "de", "du"))
 X = None
 model = AgglomerativeClustering(n_clusters = 2, linkage = "ward")
