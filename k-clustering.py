83bd031094a720851fe06c93a3d4166f379c492c
Javier Torroja <torroja.javier@gmail.com>
2026-03-17 17:11:05 +0100

    k means

--- k-clustering.py
+++ k-clustering.py
@@ -7,3 +7,9 @@ from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, linkage
 
 combinations = (("e", "u"), ("e", "u", "de"), ("e", "u", "du"), ("e", "u", "de", "du"))
+X = None
+model = AgglomerativeClustering(n_clusters = 2, linkage = "ward")
+labels = model.fit_predict(X)
+plt.scatter(X[:, 0], X[:, 1], c = labels, cmap = "viridis")
+plt.title("Agglomerative Clustering")
+plt.show()
