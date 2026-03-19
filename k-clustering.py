f30e5c2c868655c4573a8627daeec580c636959c
Javier Torroja <torroja.javier@gmail.com>
2026-03-19 10:07:04 +0100

    converted e values to rms

--- k-clustering.py
+++ k-clustering.py
@@ -5,19 +5,27 @@ from sklearn import cluster, datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram, linkage
+import json
 
 data = {}
 for subject in range(1, 7):
     globals()[f"data_P{subject}"] = []
     for condition in range(1, 7):
-        data = np.load(f"./data/python_data/ae2224I_measurement_data_subj{subject}_C{condition}.npz")
-        data_e = data["e"]
-        data_u = data["u"]
-        data_t = data["t"]
-        globals() [f"data_temp{condition}"] = [data_e, data_u, data_t]
+        data_temp = np.load(f"./data/python_data/ae2224I_measurement_data_subj{subject}_C{condition}.npz")
+        data_e = data_temp["e"]
+        data_e_array = np.array(data_e)
+        e_rms = []
+        for run in data_e_array:
+            i_th_item = data_e_array[:, run]
+            e_rms_i = np.sqrt(np.mean(i_th_item ** 2))
+            e_rms.append(e_rms_i)
+        data_u = data_temp["u"]
+        data_t = data_temp["t"]
+        globals() [f"data_temp{condition}"] = [e_rms, data_u, data_t]
         globals()[f"data_P{subject}"].append(globals() [f"data_temp{condition}"])
     data[f"Pilot{subject}"] = globals()[f"data_P{subject}"]
-print(data)
+
+data = np.array(data)
 
 
 """
