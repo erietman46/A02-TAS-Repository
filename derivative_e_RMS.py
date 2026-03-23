6ac77f0b0349e9d923de3f62088869a3e450400d
Can <canfaks@gmail.com>
2026-03-23 19:46:48 +0100

    MSE of de/dt

---
+++ derivative_e_RMS.py
@@ -0,0 +1,42 @@
+from preprocessing import pilots
+import numpy as np
+import pandas as pd
+
+#Array to store RMS errors
+errors_d = np.zeros((6,6))
+time = pilots[0]["C1"]["t"]
+
+#RMS will be calculated for all experiments and the average will be taken per condition per pilot.
+#P V A no motion then motion C1 --> C6
+for i in range(len(pilots)):
+    #looping pilots
+    pilot = pilots[i]
+
+    for j in range(len(pilot)):
+        #Condition C"k"
+        #looping conditions
+        k = j+1
+        condition = f"C{k}"
+        error = pilot[condition]["e"]
+
+        sum = 0
+
+        for l in range(5):
+            #looping columns
+            column = error[:,l]
+
+            #Taking derivative
+            derivative = np.gradient(column)
+            mean_squared = np.mean(derivative**2)
+            sum += np.sqrt(mean_squared)
+
+        MSE = sum/5
+        errors_d[i,j] = MSE
+
+#An array which the rows are pilots and columns are conditions
+df = pd.DataFrame(
+    errors_d,
+    index=[f"Pilot {i+1}" for i in range(6)],
+    columns=[f"C{i+1}" for i in range(6)]
+)
+print(f"\nMean RMS: \n{df}")
