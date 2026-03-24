1abb4291b3d2ab36bda5a8ead13eb0bfaa93e8ea
Can <canfaks@gmail.com>
2026-03-24 14:30:40 +0100

    input function

--- RMS_u.py
+++ RMS_u.py
@@ -1,33 +1,49 @@
 from preprocessing import pilots
+import statistics
 import numpy as np
-
-#Array to store RMS u
-u = np.zeros((6,6))
+import pandas as pd
 
 
-#RMS will be calculated for all experiments and the average will be taken per condition per pilot.
-#P V A no motion then motion C1 --> C6
-for i in range(len(pilots)):
-    #looping pilots
-    pilot = pilots[i]
+def RMS_input():
+    #Array to store RMS u
+    u = np.zeros((6,6))
 
-    for j in range(len(pilot)):
-        #Condition C"k"
-        #looping conditions
-        k = j+1
-        condition = f"C{k}"
-        u_0 = pilot[condition]["u"]
 
-        sum = 0
+    #RMS will be calculated for all experiments and the average will be taken per condition per pilot.
+    #P V A no motion then motion C1 --> C6
+    for i in range(len(pilots)):
+        #looping pilots
+        pilot = pilots[i]
 
-        for l in range(5):
-            #looping columns
-            column = u_0[:,l]
-            mean_squared = np.mean(column**2)
-            sum += np.sqrt(mean_squared)
+        for j in range(len(pilot)):
+            #Condition C"k"
+            #looping conditions
+            k = j+1
+            condition = f"C{k}"
+            u_0 = pilot[condition]["u"]
 
-        RMS = sum/5
-        u[i,j] = RMS
+            sum = 0
+
+            for l in range(5):
+                #looping columns
+                column = u_0[:,l]
+                mean_squared = np.mean(column**2)
+                sum += np.sqrt(mean_squared)
+
+            RMS = sum/5
+            u[i,j] = RMS
         
-#An array which the rows are pilots and columns are conditions
-print(u)
+    #An array which the rows are pilots and columns are conditions
+    df = pd.DataFrame(
+        u,
+        index=[f"Pilot {i + 1}" for i in range(6)],
+        columns=[f"C{i + 1}" for i in range(6)]
+    )
+    res = statistics.statistics(np.array(errors))
+    results = pd.DataFrame(
+        res,
+        index=[f"C{i + 1}" for i in range(3)],
+        columns=["p_val", "effect_size"]
+    )
+
+    return df, results
