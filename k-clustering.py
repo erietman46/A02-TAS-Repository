204e3d936f6325b92749fb9587b902023d0aba9e
Javier Torroja <torroja.javier@gmail.com>
2026-03-24 14:36:36 +0100

    data organisation

--- k-clustering.py
+++ k-clustering.py
@@ -9,15 +9,13 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 def dydx_rms(y, x):
     dy = np.diff(y)
     dx = np.diff(x)
-    dydx =dy / dx
+    dydx = dy / dx
     return float(np.sqrt(np.mean(dydx ** 2)))
 
 data = {}
 for subject in range(1, 7):
-    print(f"Processing subject {subject}...")
 
     for condition in range(1, 7):
-        print(f"  Processing condition {condition}...")
         data_temp = np.load(f"./data/python_data/ae2224I_measurement_data_subj{subject}_C{condition}.npz")
         data_t = data_temp["t"]
         data_t_array = np.array(data_t)
@@ -43,9 +41,15 @@ for subject in range(1, 7):
             u_rms.append(u_rms_i)
             du_dt = dydx_rms(i_th_item, data_t_array)
             du_dt_rms.append(du_dt)
-        print(f"u: {u_rms}, du/dt: {du_dt_rms}, e: {e_rms}, de/dt: {de_dt_rms}")
-
+        data[(f"P{subject}_C{condition}")] = {"e": e_rms, "u": u_rms, "de_dt": de_dt_rms, "du_dt": du_dt_rms}
 
 data = np.array(data)
+print(data)
 
 combinations = (("e", "u"), ("e", "u", "de_dt"), ("e", "u", "du_dt"), ("e", "u", "de_dt", "du_dt"))
+for condition in range(1, 7):
+    if condition 
+    for pilot in range(1, 7):
+        for combination in combinations:
+            globals ()[f"Combination_{combination}_P_{pilot}_C{condition}"] = [data(f"P{pilot}_C{condition}")[feature] for feature in combination]
+        
