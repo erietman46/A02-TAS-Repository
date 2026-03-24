37f124c0f93219ccfe64e0c30817b9f4f2da217f
Can <canfaks@gmail.com>
2026-03-24 14:42:21 +0100

    u derivative function

--- derivative_u_RMS.py
+++ derivative_u_RMS.py
@@ -1,42 +1,52 @@
 from preprocessing import pilots
 import numpy as np
 import pandas as pd
+import statistics
 
-#Array to store RMS derivative of inputs
-U_d = np.zeros((6,6))
-time = pilots[0]["C1"]["t"]
+def RMS_DERu():
+    #Array to store RMS derivative of inputs
+    U_d = np.zeros((6,6))
 
-#RMS will be calculated for all experiments and the average will be taken per condition per pilot.
-#P V A no motion then motion C1 --> C6
-for i in range(len(pilots)):
-    #looping pilots
-    pilot = pilots[i]
+    #RMS will be calculated for all experiments and the average will be taken per condition per pilot.
+    #P V A no motion then motion C1 --> C6
+    for i in range(len(pilots)):
+        #looping pilots
+        pilot = pilots[i]
 
-    for j in range(len(pilot)):
-        #Condition C"k"
-        #looping conditions
-        k = j+1
-        condition = f"C{k}"
-        U_0 = pilot[condition]["u"]
+        for j in range(len(pilot)):
+            #Condition C"k"
+            #looping conditions
+            k = j+1
+            condition = f"C{k}"
+            U_0 = pilot[condition]["u"]
 
-        sum = 0
+            sum = 0
 
-        for l in range(5):
-            #looping columns
-            column = U_0[:,l]
+            for l in range(5):
+                #looping columns
+                column = U_0[:,l]
 
-            #Taking derivative
-            derivative = np.gradient(column)
-            mean_squared = np.mean(derivative**2)
-            sum += np.sqrt(mean_squared)
+                #Taking derivative
+                derivative = np.gradient(column, 0.01)
+                mean_squared = np.mean(derivative**2)
+                sum += np.sqrt(mean_squared)
 
-        RMS = sum/5
-        U_d[i,j] = RMS
+            RMS = sum/5
+            U_d[i,j] = RMS
 
-#An array which the rows are pilots and columns are conditions
-df = pd.DataFrame(
-    U_d,
-    index=[f"Pilot {i+1}" for i in range(6)],
-    columns=[f"C{i+1}" for i in range(6)]
-)
-print(f"\nMean RMS: \n{df}")
+    #An array which the rows are pilots and columns are conditions
+    df = pd.DataFrame(
+        U_d,
+        index=[f"Pilot {i+1}" for i in range(6)],
+        columns=[f"C{i+1}" for i in range(6)]
+    )
+    print(f"\nMean RMS: \n{df}")
+
+    res = statistics.statistics(np.array(U_d))
+    results = pd.DataFrame(
+        res,
+        index=[f"C{i + 1}" for i in range(3)],
+        columns=["p_val", "effect_size"]
+    )
+
+    return df, results
